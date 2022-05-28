b64ca48c8dc03b96d5b5bb23215110fe5c82acb8
BrunoPre <brunopre44@gmail.com>
2022-05-28 11:34:07 +0100

    [6] Frontend: refacto sidebar's buttons: - create interface ButtonSidebar to parse a button's properties from a JSON config file to an object - ButtonSidebar contains all the neeeded attributes: button name, label to be displayed, route, paths to icons - add JSON config file to map buttons to their props

---
+++ src/types/ButtonSidebar.ts
@@ -0,0 +1,8 @@
+export interface ButtonSidebar {
+  index : number;
+  buttonName: string;
+  label: string;
+  iconPath: string;
+  iconBoxIcon: string; // if no icon is available in the `assets` folder, icon from BoxIcons is fetched
+  route: string;
+}
