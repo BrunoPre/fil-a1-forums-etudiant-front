191c31b767e2a1dd7c90427e9932653b5614be61
BrunoPre <brunopre44@gmail.com>
2022-06-04 10:30:13 +0100

    [20] OPS/Frontend: last lint fixes

--- src/components/ReplyComponentMocklyIntegrated.vue
+++ src/components/ReplyComponentMocklyIntegrated.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container-mock">
-    <img :src="require('./../assets/mockThread.png')" class="img-mock">
+    <img :src="require('./../assets/mockThread.png')" class="img-mock" />
     <reply-to-question-input></reply-to-question-input>
   </div>
 </template>
@@ -11,19 +11,17 @@ import ReplyToQuestionInput from "./ReplyToQuestionInput.vue";
 
 export default defineComponent({
   name: "ReplyComponentMocklyIntegrated",
-  components : {ReplyToQuestionInput},
+  components: { ReplyToQuestionInput },
 });
 </script>
 
 <style scoped>
-
-.container-mock{
-  padding : 5em;
+.container-mock {
+  padding: 5em;
 }
 
 .img-mock {
-  max-width:100%;
-  height:auto;
+  max-width: 100%;
+  height: auto;
 }
-
 </style>
--- src/components/ReplyToQuestionInput.vue
+++ src/components/ReplyToQuestionInput.vue
@@ -41,7 +41,7 @@ export default defineComponent({
       }
     },
   },
-  mounted() {},
+  //mounted() {},
 });
 </script>
 
